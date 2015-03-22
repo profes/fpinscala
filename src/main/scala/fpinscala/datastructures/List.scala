36f5f42047862e0feb701aaa1271d8568f0b0393
damian <damian@atchik.com>
2015-03-22 23:20:27 +0100

    wip

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -1,9 +1,10 @@
 package fpinscala.datastructures
 
 sealed trait List[+A]
-case object Nil extends List[Nothing]
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
   def sum(ints: List[Int]): Int = ints match {
@@ -20,4 +21,61 @@ object List {
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](ds: List[A]): List[A] = {
+    ds match {
+      case Nil => sys.error("list is nil")
+      case Cons(_, tail) => tail
+    }
+  }
+
+  def setHead[A](ds: List[A], head: A): List[A] = {
+    ds match {
+      case Nil => sys.error("list is nil")
+      case Cons(_, tail) => Cons(head, tail)
+    }
+  }
+
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n > 0)
+      l match {
+        case Nil => sys.error("list is nil")
+        case Cons(_, tail) => drop(tail, n - 1)
+      }
+    else l
+
+  }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Cons(head, tail) if f(head) => dropWhile(tail, f)
+      case _ => l
+    }
+  }
+
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => l
+      case Cons(h, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+    }
+  }
+
+  def dropWhile2[A](l: List[A])(f: A => Boolean): List[A] = {
+    l match {
+      case Cons(head, tail) if f(head) => dropWhile2(tail)(f)
+      case _ => l
+    }
+  }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+  }
+
+  def sum2(ns: List[Int]) = foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ds: List[Double]) = foldRight(ds, 1.0)(_ * _)
 }
--- src/main/scala/fpinscala/datastructures/Ws.sc
+++ src/main/scala/fpinscala/datastructures/Ws.sc
@@ -1,6 +1,7 @@
 import fpinscala.datastructures._
 
 val l = Cons(1, Nil)
+
 Cons("a", Cons("b", Nil))
 
 List(1, 2, 3) match {
@@ -10,3 +11,20 @@ List(1, 2, 3) match {
   case Cons(h, _) => h
 }
 
+List(1, 2, 3, 4, 5) match {
+  case Cons(x, Cons(2, Cons(4, _))) => x
+  case Nil => 42
+  case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+  case _ => 101
+}
+
+List.tail(List(1, 2, 3))
+List.setHead(List(1, 2, 3), 3)
+List.drop(List(1, 2, 3, 4, 5), 2)
+List.dropWhile(List(1, 2, 3, 4, 5), (x: Int) => x > 2)
+List.init(List(1, 2, 3, 4, 5))
+
+List.dropWhile2(List(1, 2, 3, 4, 5))(_ > 2)
+
+List.sum2(List(1, 2, 3))
+List.product(List(1, 2, 3))
