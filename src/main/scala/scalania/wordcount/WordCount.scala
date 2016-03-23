1f84d4e1bd742fe869cbc4ca74be916e0a7d4639
profes <profes82@gmail.com>
2016-03-23 19:12:51 +0100

    adding stuff

--- src/main/scala/scalania/wordcount/WordCount.scala
+++ src/main/scala/scalania/wordcount/WordCount.scala
@@ -1,8 +1,10 @@
 package scalania.wordcount
 
-import java.io.File
+import java.io.{File, FileWriter, PrintWriter}
 
 import scala.io.{Codec, Source}
+import sys.process._
+import scala.collection.immutable.ListMap
 
 object WordCount {
   implicit val codec = Codec.UTF8
@@ -14,19 +16,47 @@ object WordCount {
   }
 
   def wc(file: File): Map[String, Int] = {
-    Source.fromFile(file)
-      .getLines
-      .flatMap(_.split("\\W+"))
-      .foldLeft(Map.empty[String, Int]) {
-        (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
-      }
+    using(Source.fromFile(file)) { source =>
+      source.getLines
+        .flatMap(_.split("\\W+"))
+        .filterNot(_ == "")
+        .foldLeft(Map.empty[String, Int]) {
+          (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
+        }
+    }
   }
 
-  def wcs(files: Array[File]) = {
-    val maps = files.map(wc(_))
-    println(maps.toList)
-    maps.foldLeft(Map.empty[String, Int]) {
+  def wcFiles(files: Array[File]): Map[String, Int] = {
+    files.map(wc(_)).foldLeft(Map.empty[String, Int]) {
       (left, right) => (left.keySet ++ right.keySet).map(i => (i, left.getOrElse(i, 0) + right.getOrElse(i, 0))).toMap
     }
   }
+
+  def listHashes(dir: File): Array[String] = {
+    val lines = Seq("git", "-C", dir.getAbsolutePath, "log", "--pretty=format:\"%h\"").!!
+    lines.split("\n").map(_.replace("\"", "")).reverse
+  }
+
+  def checkout(dir: File, hash: String) = {
+    val lines = Seq("git", "-C", dir.getAbsolutePath, "checkout", hash).!!
+  }
+
+  def run(dir: File, take: Int = 10) = {
+    listHashes(dir).take(take).foreach(hash => {
+      checkout(dir, hash)
+      val wcs = wcFiles(listFiles(dir))
+
+      val sorted = ListMap(wcs.toSeq.sortWith(_._2 > _._2):_*)
+      using(new FileWriter(s"/home/dp/projects/tmp/$hash")) { pw =>
+        pw.write(sorted.mkString("\n"))
+      }
+    })
+  }
+
+  def using[A <: {def close() : Unit}, B](param: A)(f: A => B): B =
+    try {
+      f(param)
+    } finally {
+      param.close()
+    }
 }
--- src/main/scala/scalania/wordcount/ws.sc
+++ src/main/scala/scalania/wordcount/ws.sc
@@ -1,10 +1,19 @@
 import java.io.File
 
+import scala.collection.immutable.ListMap
 import scalania.wordcount.WordCount
 
-val f = new File("/home/dp/projects/fpinscala/src/main")
+val f = new File("/home/dp/projects/fpinscala-book")
 val files = WordCount.listFiles(f).sorted
 
 WordCount.wc(files(0))
 
-WordCount.wcs(files)
+val count = WordCount.wcFiles(files)
+
+ListMap(count.toSeq.sortWith(_._2 > _._2):_*)
+
+//WordCount.listHashes(f)
+
+//WordCount.checkout(f, "d763887")
+import scalania.wordcount.WordCount
+WordCount.run(f)
